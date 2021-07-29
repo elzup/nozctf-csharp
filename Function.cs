888f435e3856ee81b20e6135edfd2d9af622a1df
elzup <guild0105@gmail.com>
2021-07-29 10:09:34 +0900

    udpate req

--- Function.cs
+++ Function.cs
@@ -3,32 +3,39 @@ using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System;
+using System.IO;
 
-namespace nozctf_csharp {
-  public class Function: IHttpFunction {
-    /// <summary>
-    /// Logic for your function goes here.
-    /// </summary>
-    /// <param name="context">The HTTP context, containing the request and the response.</param>
-    /// <returns>A task representing the asynchronous operation.</returns>
-    public async Task HandleAsync(HttpContext context) {
-      HttpRequest request = context.Request;
-      HttpResponse response = context.Response;
+namespace nozctf_csharp
+{
+	public class Function : IHttpFunction
+	{
+		/// <summary>
+		/// Logic for your function goes here.
+		/// </summary>
+		/// <param name="context">The HTTP context, containing the request and the response.</param>
+		/// <returns>A task representing the asynchronous operation.</returns>
+		public async Task HandleAsync(HttpContext context)
+		{
+			HttpRequest request = context.Request;
+			HttpResponse response = context.Response;
+			using TextReader reader = new StreamReader(request.Body);
 
-      string pattern = context.Request.Body.AsString();
-      string sentence = "X CAN YOU SEE ANYTHING Q";
+			// length check
+			string pattern = await reader.ReadLineAsync();
+			string sentence = "X CAN YOU SEE ANYTHING Q";
 
-      try {
-        foreach(Match match in Regex.Matches(sentence, pattern,
-          RegexOptions.None,
-          TimeSpan.FromSeconds(1))) {
-          await response.WriteAsync(match.Value);
-        }
-      } catch (RegexMatchTimeoutException) {
-        string secret = Environment.GetEnvironmentVariable("KEY_Q7") ?? "";
+			try {
+				foreach (Match match in Regex.Matches(sentence, pattern,
+					RegexOptions.None,
+					TimeSpan.FromSeconds(1))) {
+					await response.WriteAsync(match.Value);
+				}
+			}
+			catch (RegexMatchTimeoutException) {
+				string secret = Environment.GetEnvironmentVariable("KEY_Q7") ?? "";
 
-        await response.WriteAsync($"Found 'FLAG_{secret}'");
-      }
-    }
-  }
+				await response.WriteAsync($"TIMEOUT! 'FLAG_{secret}'");
+			}
+		}
+	}
 }
